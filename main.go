e68d10fdd0796010a99efc9e6681e498f5964318
amin.mousavi <amin.mousavi@unity3d.com>
2025-01-07 08:04:27 +0200

    Added a custom Handler

--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a custom handler called MyHandler!")
+}
+
 func main() {
+	// Using HandleFunc for root address
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
 
+	// Use a CustomHandler which implements the ServeHTTP interface:
+	// type Handler interface {
+	// 	ServeHTTP(ResponseWriter, *Request)
+	// }
+	handler := &MyHandler{}
+	http.Handle("/customHandler", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
